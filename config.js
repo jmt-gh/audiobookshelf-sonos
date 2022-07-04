265ca47658794b87c327ce9ffb77ca16afe8ed23
jmt-gh <jmt-gh>
2022-07-04 09:27:24 -0700

    remove default port

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
 module.exports = Object.freeze({
-   HTTP_PORT: 3333, // what port to for express to listen on. Where your SOAP && HTTP requests should end up
+   HTTP_PORT: , // what port to for express to listen on. Where your SOAP && HTTP requests should end up
    SOAP_ENDPOINT: '/wsdl', // what endpoint sonos will reach out to. Defined in the custom service descriptor
    SONOS_WSDL_FILE: fs.readFileSync('sonos.wsdl', 'utf8'),
    SOAP_URI: '', // https://yoursoap.url.com
