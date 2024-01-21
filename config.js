b01aa09a3f313891eeb00bca71d592bf38a511ab
jmt-gh <13617455+jmt-gh@users.noreply.github.com>
2024-01-20 22:45:36 -0800

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports = Object.freeze({
   SOAP_ENDPOINT: "/wsdl", // what endpoint sonos will reach out to. Defined in the custom service descriptor
   SONOS_WSDL_FILE: fs.readFileSync("sonos.wsdl", "utf8"),
   SOAP_URI: process.env.SOAP_URI, // https://yoursoap.url.com
-  ABS_URI: process.env.ABS_URI, // http://your.url.com
-  ABS_LIBRARY_ID: process.env.ABS_LIBRARY_ID, // lib_*****
+  ABS_URI: process.env.ABS_URI, // http://yourabs.url.com
+  ABS_LIBRARY_ID: process.env.ABS_LIBRARY_ID, // UUID-style library ID
   ABS_TOKEN: process.env.ABS_TOKEN, // ABS -> settings -> users -> select user -> API TOKEN
 });
